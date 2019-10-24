42bea756cbe16b7fde8fe6228b568fb2c5e698cd
Aleksandr Shchigrintsov <aleksandr.s@starofservice.com>
2019-10-24 17:47:00 +0200

    Initial commit, base structure

---
+++ readme.md
@@ -0,0 +1,11 @@
+# A Hangman game on Vue.js
+
+Hangman game builded on Vue.js
+
+# For run locally
+
+```
+cd hangman
+npm install
+npm run development
+```
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- Components -->
+  </div>  
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data: function () {
+      return {}
+    },
+    methods: {},
+    components: {}
+  }
+</script>
