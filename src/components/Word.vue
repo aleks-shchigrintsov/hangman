a4359aec40a883e80c9d0a8b8f3e7128cbac1f97
Aleksandr Shchigrintsov <aleksandr.s@starofservice.com>
2019-10-27 13:27:18 +0100

    Word component

---
+++ src/components/Word.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="word-container">
+    <div
+      class="letter"
+      v-for="(letter, index) in wordDisplayLetters"
+      v-bind:key="index"
+    >
+      {{ letter }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Word',
+    data: function () {
+      return {}
+    },
+    props: {
+      wordDisplayLetters: Array,
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import '../styles/typography.scss';
+
+  .letter {
+    display: inline-block;
+    border-bottom: 1px solid;
+    margin: 0 3px;
+    font-size: $font-size-title;
+    min-width: 30px;
+    vertical-align: bottom;
+  }
+
+  .word-container {
+    margin-bottom: 20px;
+  }
+</style>
